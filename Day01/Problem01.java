e9724f122893c066185d433afb47ee131127b06f
Sandeep Rishi <123352611+18Sandeep18@users.noreply.github.com>
2024-06-10 22:11:32 +0530

    Update Problem01.java
    Update k value by applying mod(%) with array size, so that k will never go out of bound

--- Day01/Problem01.java
+++ Day01/Problem01.java
@@ -16,6 +16,7 @@ public class RotateArrayByK {
         // Modify input values and make it custom by adding scanner class
         int arr[] = { 4, 1, 6, 9, 2, 14, 7, 8, 3 };
         int k = 6;
+        k = k%arr.length;
         reverse(arr, 0, arr.length - 1);
         reverse(arr, 0, k - 1);
         reverse(arr, k, arr.length - 1);
