6e85885fff77255816f20dff4ecac2c5b3e6d8b1
Sandeep Rishi <rishisandeep8@gmail.com>
2024-06-11 12:08:13 +0530

    Problem01

--- Day01/RotateArrayByK.java
+++ Day01/RotateArrayByK.java
@@ -1,11 +1,12 @@
 import java.util.Arrays;
 
-public class Problem01 {
+class RotateArrayByK {
 
     public static void reverse(int arr[], int s, int e) {
         while (s < e) {
             int temp = arr[s];
             arr[s] = arr[e];
+
             arr[e] = temp;
             s++;
             e--;
@@ -13,13 +14,15 @@ public class Problem01 {
     }
 
     public static void main(String[] args) {
-        // Modify input values and make it custom by adding scanner class
-        int arr[] = { 4, 1, 6, 9, 2, 14, 7, 8, 3 };
-        int k = 6;
-        k = k%arr.length;
-        reverse(arr, 0, arr.length - 1);
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int k = 5;
+
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
         reverse(arr, 0, k - 1);
-        reverse(arr, k, arr.length - 1);
+        reverse(arr, k, n - 1);
 
         System.out.println(Arrays.toString(arr));
     }
