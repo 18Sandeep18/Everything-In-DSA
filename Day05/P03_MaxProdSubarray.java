45f4917d7d5033351b573ea74aa8526404ea5e2a
Sandeep Rishi <rishisandeep8@gmail.com>
2024-06-13 22:45:25 +0530

    Problem03 Code

---
+++ Day05/P03_MaxProdSubarray.java
@@ -0,0 +1,23 @@
+class Solution3 {
+    public static int maxProduct(int[] nums) {
+        int pp = 1, sp = 1, n = nums.length, max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (pp == 0)
+                pp = 1;
+            if (sp == 0)
+                sp = 1;
+
+            pp = pp * nums[i];
+            sp = sp * nums[n - i - 1];
+
+            max = Math.max(max, Math.max(pp, sp));
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        // Take custom input by adding scanner class
+        int arr[] = { 1, 2, -5, -4, 3, -2, 4, 5 };
+        System.out.println(maxProduct(arr));
+    }
+}
