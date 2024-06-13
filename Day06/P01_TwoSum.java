df41f83623b2211de51da35bf825209209311c9b
Sandeep Rishi <rishisandeep8@gmail.com>
2024-06-13 23:03:03 +0530

    Problem01 Code

---
+++ Day06/P01_TwoSum.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public static int[] twoSum(int arr[], int target) {
+
+        int i = 0, n = arr.length, j = n - 1;
+
+        while (i < j) {
+            int k = arr[i] + arr[j];
+            if (k == target) {
+                return new int[] { i, j };
+            } else if (k < target)
+                i++;
+            else
+                j--;
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        // Take custom input by adding scanner class
+        int arr[] = { 1, 5, 7, 12, 19 };
+        int ans[] = twoSum(arr, 19);
+        System.out.println(ans[0] + " " + ans[1]);
+    }
+}
