a1fca0ab191c14826a77f1a899a571f5e2612983
Sandeep Rishi <rishisandeep8@gmail.com>
2024-06-16 17:00:04 +0530

    Problem03 Code

---
+++ Day06/P03_ContainerWithMostWater.java
@@ -0,0 +1,30 @@
+class Solution03 {
+    public static int maxArea(int[] height) {
+        int max = Integer.MIN_VALUE;
+
+        int i = 0, n = height.length, j = n - 1;
+
+        while (i < j) {
+            // height //width
+            int area = Math.min(height[i], height[j]) * (j - i);
+
+            // updating max area
+            max = Math.max(area, max);
+
+            // if value in i is lesser update i because,
+            // that value can't help us in getting max value of area
+            if (height[i] < height[j])
+                i++;
+            else
+                j--;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        // Take custom input by adding scanner class
+        int arr[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        System.out.println(maxArea(arr));
+    }
+
+}
