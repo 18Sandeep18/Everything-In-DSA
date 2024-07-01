257e09c184ab8cf35e4a2f2ff626565e1197951b
Sandeep Rishi <rishisandeep8@gmail.com>
2024-07-01 21:39:21 +0530

    Problem 01 Updated

--- Day08/P01_SubarrayWithSumK.java
+++ Day08/P01_SubarrayWithSumK.java
@@ -1,7 +1,27 @@
-public class P01_SubarrayWithSumK {
+import java.util.HashMap;
+
+class Solution01 {
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int n = nums.length, prefixSum = 0, count = 0;
+
+        for (int i = 0; i < n; i++) {
+            prefixSum += nums[i];
+            int remainingSum = prefixSum - k;
+
+            if (map.containsKey(remainingSum)) {
+                count += map.getOrDefault(remainingSum, 0);
+            }
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = 50;
-        int b = 100;
+        int arr[] = { 1, 2, 3, 4, 1 };
+        int k = 5;
+        System.out.print(subarraySum(arr, k));
     }
 }
